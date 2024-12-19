ccfe27de8c90ec16dc1141627ff602707d7e7559
sajid <patansajidalikhan51@gmail.com>
2024-12-19 08:27:22 +0530

    add render function to script.js

--- src/script.js
+++ src/script.js
@@ -1,94 +1,128 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //main buttons
+    const startBtn = document.getElementById('startButton')
+    const startMainBtn = document.getElementById('startMain')
+    const clearButton = document.getElementById('clear')
 
-//main buttons
-const startBtn = document.getElementById('startButton')
-const startMainBtn = document.getElementById('startMain')
-const clearButton = document.getElementById('clear')
+    //sections
+    const startSection = document.getElementById('start')
+    const askingSection = document.getElementById('asking')
+    const mainSection = document.getElementById('main')
 
-//sections
-const startSection = document.getElementById('start')
-const askingSection = document.getElementById('asking')
-const mainSection = document.getElementById('main')
+    //expenses list container
+    const expenseList = document.getElementById('expensesList')
 
-//expenses list container
-const expenseList = document.getElementById('expenseList')
-
-//inputs
-const addExpense = document.getElementById('addExpense')
-const isOutGoingCheckbox = document.getElementById('isOutgoing')
-const amountInput = document.getElementById('amount')
-const costInput = document.getElementById('cost')
-const reasonInput = document.getElementById('reason')
-
-
-const expenses = JSON.parse(localStorage.getItem('expenses')) || []
-//placeholders
-const initAmount = JSON.parse(localStorage.getItem('initItem')) || 0
-const remAmount = JSON.parse(localStorage.getItem('remAmount')) || 0
-
-initAmount = parseFloat(initAmount)
-remAmount = parseFloat(remAmount)
+    //inputs
+    const addExpense = document.getElementById('addExpense')
+    const isOutGoingCheckbox = document.getElementById('isOutgoing')
+    const amountInput = document.getElementById('amount')
+    const costInput = document.getElementById('cost')
+    const reasonInput = document.getElementById('reason')
 
 
-if (initAmount !== 0 || remAmount !== 0){
-    askingSection.classList.add('hidden')
-    startSection.classList.add('hidden')
-    mainSection.classList.remove('hidden')
+    let expenses = JSON.parse(localStorage.getItem('expenses')) || []
+    //placeholders
+    let initAmount = parseFloat(JSON.parse(localStorage.getItem('initItem'))) || 0.0
+    let remAmount = parseFloat(JSON.parse(localStorage.getItem('remAmount'))) || 0
 
-    renderExpenses()
-}
+    if (initAmount !== 0 || remAmount !== 0) {
+        askingSection.classList.add('hidden')
+        startSection.classList.add('hidden')
+        mainSection.classList.remove('hidden')
 
-startBtn.addEventListener('click', () => {
-    startSection.classList.add('hidden')
-    mainSection.classList.add('hidden')
-    askingSection.classList.remove('hidden')
-})
-
-startMainBtn.addEventListener('click', () => {
-    const amount = 
-    askingSection.classList.add('hidden')
-    startSection.classList.add('hidden')
-    mainSection.classList.remove('hidden')
-})
-
-clearButton.addEventListener('click', () => {
-    askingSection.classList.add('hidden')
-    mainSection.classList.add('hidden')
-    startSection.classList.remove('hidden')
-})
-
-addExpense.addEventListener('click', () => {
-    const cost = costInput.value.trim()
-    const reason = reasonInput.value.trim()
-    const isOutgoing = isOutGoingCheckbox.checked
-
-    if(!cost || !reason || isNaN(cost)){
-        return;
+        renderExpenses()
     }
 
-    const time = Date.now('dd-mm-yyyy')
-    
-
-})
-
-function saveState(){
-    localStorage.setItem('initAMount', JSON.stringify(initAmount))
-    localStorage.setItem('remAmount', JSON.stringify(remAmount))
-    localStorage.setItem('expenses', JSON.stringify(expenses))
-}
-
-function renderExpenses(){
-    //create a li for each expense
-    expenses.forEach(expense => {
-        const li = createExpenseElement(expense)
-
-        //add to the expense list ui
-        expenseList.appendChild(li);
+    startBtn.addEventListener('click', () => {
+        startSection.classList.add('hidden')
+        mainSection.classList.add('hidden')
+        askingSection.classList.remove('hidden')
     })
-}
 
-function createExpenseElement(exp){
+    startMainBtn.addEventListener('click', () => {
+        const amount = parseFloat(amountInput.value.trim());
+        if (amount === 0) {
+            return;
+        }
+        askingSection.classList.add('hidden')
+        startSection.classList.add('hidden')
+        mainSection.classList.remove('hidden')
+    })
 
-}
+    clearButton.addEventListener('click', () => {
+        remAmount = 0;
+        initAmount = 0;
+        expenses = []
+        saveState();
+        askingSection.classList.add('hidden')
+        mainSection.classList.add('hidden')
+        startSection.classList.remove('hidden')
+    })
+
+    addExpense.addEventListener('click', () => {
+        const cost = costInput.value.trim()
+        const reason = reasonInput.value.trim()
+        const isOutgoing = isOutGoingCheckbox.checked
+
+        if (!cost || !reason || isNaN(cost)) {
+            return;
+        }
+
+        const today = new Date();
+
+        const date = today.toLocaleDateString('en-GB'); 
+
+        const expenseObject = {
+            time: date,
+            cost,
+            reason,
+            isOutgoing
+        }
+
+        console.log(expenseObject)
+        expenses.push(expenseObject)
+        renderExpenses()
+    })
+
+    function saveState() {
+        localStorage.setItem('initAMount', JSON.stringify(initAmount))
+        localStorage.setItem('remAmount', JSON.stringify(remAmount))
+        localStorage.setItem('expenses', JSON.stringify(expenses))
+    }
+
+    function renderExpenses() {
+        expenseList.innerHTML = "";
+        //create a li for each expense
+        for(let i = expenses.length - 1; i >= 0; i--){
+            const expense = expenses[i]
+
+            const li = createExpenseElement(expense)
+
+            console.log(li)
+            expenseList.appendChild(li)
+        }
+        saveState();
+    }
+
+    function createExpenseElement(exp) {
+        const li = document.createElement('li');
+
+        const timeDiv = document.createElement('div')
+        const reasonDiv = document.createElement('div')
+        const costDiv = document.createElement('div')
+
+        timeDiv.textContent = exp.time;
+        reasonDiv.textContent = exp.reason;
+        exp.cost = parseFloat(exp.cost)
+        costDiv.textContent = (exp.isOutgoing) ? -exp.cost : exp.cost
+
+        li.appendChild(timeDiv)
+        li.appendChild(reasonDiv)
+        li.appendChild(costDiv)
+
+        return li
+    }
+})
 
 
 
